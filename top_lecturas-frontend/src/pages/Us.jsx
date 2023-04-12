e506d0bce30a8bd10ff446c6d1ce32941fcca641
Cecilia-15 <ceciliafontana1505@gmail.com>
2023-04-12 19:20:50 -0300

    vista de nosotros

--- top_lecturas-frontend/src/pages/Us.jsx
+++ top_lecturas-frontend/src/pages/Us.jsx
@@ -1,7 +1,58 @@
 import React from 'react'
+import TitleH1 from 'src/components/TitleH1'
+import Icon from 'src/components/Icon'
+import { Link } from 'react-router-dom'
+
+const US_DATA = [
+    {
+        name: 'Cecilia',
+        lastName: 'Fontana',
+        gitLink: 'https://github.com/Cecilia-15',
+        linLink: 'https://www.linkedin.com/in/cecilia-fontana-84001a1b6/'
+    },
+    {
+        name: 'Manuel',
+        lastName: 'Chamorro',
+        gitLink: 'https://github.com/VMchamo',
+        linLink: 'https://www.linkedin.com/in/vmchamorro/'
+    },
+    {
+        name: 'Nicolás',
+        lastName: 'Moreira',
+        gitLink: 'https://github.com/nicom700',
+        linLink: 'https://www.linkedin.com/in/nicolasmoreira3/'
+    },
+    {
+        name:'Daniel', 
+        lastName: 'Billordo',
+        gitLink: 'https://github.com/dabifer',
+        linLink: ''
+    }
+]
 
 export default function Us () {
   return (
-    <div>us</div>
+    <div className='my-4 max-md:px-2 px-8 w-full flex flex-col items-center gap-4'>
+        <TitleH1 text='Sobre nosotros'/>
+        <div className='max-w-3xl w-full flex justify-between'>
+            {
+                US_DATA.map(data => (
+                    <div className='max-w-3xl w-full gap-4 max-sm:gap-2 text-gray-700 dark:text-gray-300 bg-white dark:bg-zinc-800 p-6 shadow-md rounded-xl mx-1.5 text-center flex	flex-col items-center'>
+                        <h2 className='text-lg font-bold my-2' >{data.name} <br/> {data.lastName}</h2>
+                        <div className='flex justify-center'>
+                            <Link to={data.gitLink}>
+                                <Icon icon='gitHubIcon'/>
+                            </Link>
+                            <Link to={data.linLink}>
+                                <Icon icon='linkedInIcon'/>
+                            </Link>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+        
+        
+    </div>
   )
 }
